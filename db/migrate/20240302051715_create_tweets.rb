ef8f14aae925aa2d14dad36af98a3948278a2951
morieeeenyo <uint2435@gmail.com>
2024-03-02 14:20:51 +0900

    chore: migration実行

--- db/migrate/20240302051715_create_tweets.rb
+++ db/migrate/20240302051715_create_tweets.rb
@@ -2,6 +2,7 @@ class CreateTweets < ActiveRecord::Migration[7.1]
   def change
     create_table :tweets do |t|
 
+      t.string :content, null: false, comment: "ツイートの内容"
       t.timestamps
     end
   end
